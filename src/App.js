fb0a9269be3394b7ba67648ebeb44a2e2234f866
ClaudiuHancu <claudiuhancu@gmail.com>
2021-04-25 23:58:01 +0300

    salvare

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import HomePage from "./homepage.component";
+
 function App() {
-  return <div>Claudiu</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
